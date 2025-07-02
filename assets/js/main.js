2cfce0479c444da043d4330d82bd05f3c94ddf74
SARTIP1388 <amirhessamsarkhanzadeh@gmail.com>
2025-07-02 17:50:11 +0330

    Part #4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
-}); 
+});
+document.addEventListener('DOMContentLoaded', function () {
+    var elemsDropdown = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(elemsDropdown, {
+        // برای sidenav باید گزینه coverTrigger رو false بذاریم
+        coverTrigger: false,
+        constrainWidth: false,
+    });
+
+    var elemsSidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elemsSidenav);
+});
